3302e75fbb6de7e9f174848ea1e9a1f1ea60eb02
Evgeniy Budaev <budaev.e@gmail.com>
2023-02-09 07:45:09 +0300

    added to serverDependenciesToBundle a @ant-design/plots and d3-interpolate

--- remix.config.js
+++ remix.config.js
@@ -1,6 +1,7 @@
 /** @type {import('@remix-run/dev').AppConfig} */
 module.exports = {
   ignoredRouteFiles: ["**/.*"],
+  serverDependenciesToBundle: ["@ant-design/plots", "d3-interpolate"],
   // appDirectory: "app",
   // assetsBuildDirectory: "public/build",
   // serverBuildPath: "build/index.js",
